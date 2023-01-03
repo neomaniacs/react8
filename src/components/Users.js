592fa137e5621f517812c33917ff15c37a4005db
Laurent FIDAHOUSSEN <10000.maniacs@gmail.com>
2023-01-03 09:18:26 +0100

    last updates

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,14 +1,20 @@
-import { useState, Component } from "react";
+import { Component } from "react";
 import User from "./User";
 
 import classes from "./Users.module.css";
 
 class Users extends Component {
   constructor() {
-    super() ;
+    super();
     this.state = {
       showUsers: true,
     };
+  }
+
+  componentDidUpdate() {
+    if(this.props.users.length === 0){
+      throw new Error('No users provided') ;
+    }
   }
 
   toggleUsersHandler() {
